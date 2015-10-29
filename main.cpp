cfbd6301e55d4aa6b50a8b0f9ee2de3a983debb6
alantea <alan094@gmail.com>
2015-10-29 23:49:30 +0800

    add test is triangle unit

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 
 int triangle(int a, int b ,int c)
 {
+	// first part
+	if( !(a >= 1 && a <= 200 ) )
+	{
+		return -1;
+	}
+	else if( !(b >= 1 && b <= 200 ) )
+	{
+		return -1;
+	}
+	else if( !(c >= 1 && c <= 200 ) )
+	{
+		return 0;
+	}
 
+
+	return 0;
 }
--- main_unittest.cpp
+++ main_unittest.cpp
@@ -5,25 +5,36 @@ TEST(TriangleTest, HandleNoneZeroInput)
 {
 	// Return value
 	// -1 : is not in range
-	// 0 : is triangle
+	// -2 : is not a triangle
 
-	// test the line is in the define
 	
 	// test c
     EXPECT_EQ(-1, triangle(1, 1, 0));
     EXPECT_EQ(-1, triangle(1, 1, 201));
-    EXPECT_EQ(0, triangle(1, 1, 1));
-    EXPECT_EQ(0, triangle(1, 1, 200));
 
 	// test b
     EXPECT_EQ(-1, triangle(1, 0, 1));
     EXPECT_EQ(-1, triangle(1, 201, 1));
-    EXPECT_EQ(0, triangle(1, 2, 1));
-    EXPECT_EQ(0, triangle(1, 200, 1));
 
 	// test a
     EXPECT_EQ(-1, triangle(0, 1, 1));
     EXPECT_EQ(-1, triangle(201, 1, 1));
-    EXPECT_EQ(0, triangle(2, 1, 1));
-    EXPECT_EQ(0, triangle(200, 1, 1));
+
+	//  test triangle
+
+	// a + b <= c
+    EXPECT_EQ(-2, triangle(2, 6, 10));
+    EXPECT_EQ(-2, triangle(1, 3, 5));
+    EXPECT_EQ(-2, triangle(2, 3, 5));
+	
+	// b + c <= a
+    EXPECT_EQ(-2, triangle(100, 20, 60));
+    EXPECT_EQ(-2, triangle(200, 198, 1));
+    EXPECT_EQ(-2, triangle(200, 198, 2));
+
+	// a + c <= b
+    EXPECT_EQ(-2, triangle( 10, 200, 20));
+    EXPECT_EQ(-2, triangle( 99, 200, 99));
+    EXPECT_EQ(-2, triangle( 100, 200, 100));
+
 }
