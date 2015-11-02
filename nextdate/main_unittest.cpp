e9df53b495a13a7b6a799792c28f04c52a05b76a
alantea <alan094@gmail.com>
2015-11-02 16:37:20 +0800

    add day check in different month

--- nextdate/main_unittest.cpp
+++ nextdate/main_unittest.cpp
@@ -57,19 +57,53 @@ TEST(NextDateTest, HandleNoneZeroInput)
 	// test in valid date
 
 	// year
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, 1, 1811));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, 1, 2013));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, 1, 0));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, 1, -1));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, 1, 10000));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 1, 1811));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 1, 2013));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 1, 0));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 1, -1));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 1, 10000));
 	
 	// month
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 0, 1, 1812));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 13, 1, 1812));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( -1, 1, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 0, 1, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 13, 1, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( -1, 1, 1812));
 
 	// day
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, 0, 1812));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, 32, 1812));
-	EXPECT_PRED_FORMAT4( CheckDate, 1, 2 , -1, nextdate( 1, -1, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 0, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 32, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, -1, 1812));
+
+	// month 1,3,5,7,8,10,10 ->  day <= 31
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 1, 32, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 3, 32, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 5, 32, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 7, 32, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 8, 32, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 10, 32, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 12, 32, 1812));
+
+	// month 4,6,9,11 -> day <= 30
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 4, 31, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 6, 31, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 9, 31, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 11, 31, 1812));
+	
+	// month 2 -> 
+	// leap year -> day <= 29
+	//	   (mod 4 = 0 and mod 100 != 0 ) or (mod 400 = 0 )
+
+	// other	 -> day <= 28
+
+	// leap test
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 30, 1812));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 30, 1816));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 30, 2012));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 30, 2000));
+	// other test
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 29, 1813));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 29, 1814));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 29, 1900));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 29, 2011));
+	EXPECT_PRED_FORMAT4( CheckDate, 0, 0 , -1, nextdate( 2, 29, 1999));
+
 }
